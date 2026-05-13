4b62f332f745c2710daf540e788f5c6f7400794e
Stefan Moeller <stefan@oeapps.de>
2026-05-13 19:23:33 +0200

    Add config-entry-only schema for Hassfest

--- custom_components/stundenplan/__init__.py
+++ custom_components/stundenplan/__init__.py
@@ -5,10 +5,12 @@ from typing import Any
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN
 
 PLATFORMS: tuple[Platform, ...] = (Platform.SENSOR,)
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 def entry_config(entry: ConfigEntry) -> dict[str, Any]:
